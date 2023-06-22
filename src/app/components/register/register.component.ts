ad215a18828e660482312090a37076cce449ea90
Lvkoena github <lebodigitalacademy>
2023-06-22 18:39:35 +0200

    register page form validation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
-  firstName: any;
-
+export class RegisterComponent {
+  confirmPass: string = 'none';
   constructor() {}
-
-  ngOnInit(): void {}
 
   registerForm = new FormGroup({
     name: new FormControl('', [
@@ -19,13 +16,46 @@ export class RegisterComponent implements OnInit {
       Validators.minLength(3),
       Validators.pattern('[a-zA-Z].*'),
     ]),
+    surname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.pattern('[a-zA-Z].*'),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    choosePassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(16),
+    ]),
+    retypePassword: new FormControl('',[
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(16)
+    ]),
   });
 
-  //   get firstName(): FormControl {
-  //     return this.registerForm.get('firstName') as FormControl;
-  //   }
-
   onSubmit() {
-    throw new Error('Method not implemented.');
+
   }
+
+  get firstName(): FormControl {
+    return this.registerForm.get('name') as FormControl;
+  }
+
+  get lastName(): FormControl {
+    return this.registerForm.get('surname') as FormControl;
+  }
+
+  get email(): FormControl {
+    return this.registerForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.registerForm.get('password') as unknown as FormControl;
+  }
+
+  get confirmPassword(): FormControl {
+    return this.registerForm.get('confirmPassword') as unknown as FormControl;
+  }
+
 }
