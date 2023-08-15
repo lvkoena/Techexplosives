e1c3aed5750f44249ff9970e5fdbe606539d58bb
lebohang <lvkoena@gmail.com>
2023-08-15 20:21:58 +0200

    updated product service file and product details get one

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit{
+  product: any;
+
+  constructor(private route: ActivatedRoute, private productService: ProductService){
+
+  }
+
+  ngOnInit(){
+    this.individualProduct();
+  }
+
+  individualProduct():void {
+    const id: any = this.route.snapshot.paramMap.get('id');
+    this.productService.getOneProduct(id).subscribe(
+      (data) => {
+        this.product = data;
+      },
+      (error) =>{
+        console.error('Error fetching product:', error);
+      }
+    );
+  }
 
 }
