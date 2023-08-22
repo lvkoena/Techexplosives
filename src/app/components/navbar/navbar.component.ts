e14d85dfe6bbfa8f05f5b1691bce5acb407656f8
lebohang <lvkoena@gmail.com>
2023-08-22 20:18:39 +0200

    updated cart and product service

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { count } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  cartItemCount: number = 0;
+
+  constructor(private cartService: CartService) {
+  }
+
+  ngOnInit(): void {
+  }
 
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Product } from '../interface/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  private cartItems: any [] = [];
+  private cartItemsSubject = new BehaviorSubject<Product[]>([]);
+  private quantitySubject = new BehaviorSubject<number>(0);
+  private countSubject = new BehaviorSubject<number>(0);
+  private totalPriceSubject = new BehaviorSubject<number>(0);
 
   constructor() { }
 
-//   clearCart() {
-//     this.cartItems = [];
-//     this.updateCartState();
-//   }
+  addToCart(product: any) {
+    // const cartItem = this.cartItems.find(); 
+  }
 }
