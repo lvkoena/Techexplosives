29b9ca825cebc0c01d6378388d17c4f0da1ed10a
lebohang <lvkoena@gmail.com>
2023-09-18 20:09:41 +0200

    created search bar functionality

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,28 +1,41 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { count } from 'rxjs';
+import { Product } from 'src/app/interface/product';
 import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit{
-  cartItemCount: number = 0;
-  searchQuery: string = '';
-  @Output() searchQueryChange = new EventEmitter<string>();
+export class NavbarComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor(private cartService: CartService) {
-  }
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.cartService.getCartItemCount().subscribe(count =>{
-      this.cartItemCount = count;
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.productService.getAllProducts().subscribe(products => {
+      this.products = products;
     });
   }
 
-  onSearch(): void {
-    this.searchQueryChange.emit(this.searchQuery);
+  //Function to handle search
+  onSearch(query: string) {
+    if(query.trim() !== '') {
+      this.productService.searchProducts(query).subscribe(products =>{
+        this.products = products;
+      });
+    }else{
+      //If the search input is empty, load all products
+      this.productService.getAllProducts().subscribe(products =>{
+        this.products = products;
+      });
+    }
   }
-
 }
