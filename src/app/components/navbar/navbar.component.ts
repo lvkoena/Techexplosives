00928e65f3bbee1c9dd2db18891d5801004d7377
Lvkoena github <lebodigitalacademy>
2023-09-27 23:21:16 +0200

    code formatting and cleaning

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,10 +11,13 @@ import { ProductService } from 'src/app/services/product.service';
 export class NavbarComponent implements OnInit {
     products: Product[] = [];
     searchQuery: string = '';
-    searchResults: any[] = []; //Initialize an array to store search results
-    cartItemCount: number = 0; //Initialize cart item count to 0
+    searchResults: any[] = []; 
+    cartItemCount: number = 0; 
 
-    constructor(private productService: ProductService, private cartService: CartService) { }
+    constructor(
+        private productService: ProductService,
+        private cartService: CartService
+    ) { }
 
     ngOnInit(): void {
         this.cartCount();
@@ -27,14 +30,6 @@ export class NavbarComponent implements OnInit {
         });
     }
 
-
-    //Function to handle cart item count
-    cartCount() {
-        this.cartService.getCartItemCount().subscribe(count => 
-            this.cartItemCount = count
-            );
-    }
-
     //Function to handle search functionality as the user types
     onSearch() {
         //Filter items based on the search query
@@ -43,6 +38,13 @@ export class NavbarComponent implements OnInit {
         );
 
         // Log the search results to the console
-    console.log('Search Results:', this.searchResults);
+        console.log('Search Results:', this.searchResults);
+    }
+
+     //Function to handle cart item count
+     cartCount() {
+        this.cartService
+            .getCartItemCount()
+            .subscribe((count) => (this.cartItemCount = count));
     }
 }
