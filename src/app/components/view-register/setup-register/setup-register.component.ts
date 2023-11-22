5cbe637a78e160bbd32dc17ce9610496497707dc
Lvkoena github <lebodigitalacademy>
2023-11-22 21:43:33 +0200

    created setup register modules

---
+++ src/app/components/view-register/setup-register/setup-register.component.ts
@@ -0,0 +1,7 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'app-setup-register',
+    templateUrl: './setup-register.component.html'
+})
+export class SetupRegisterComponent {}
