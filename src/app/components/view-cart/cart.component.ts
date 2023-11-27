705fa887b37fad458b65edf54fd21f3dc79b03b2
Lvkoena github <lebodigitalacademy>
2023-11-27 19:19:27 +0200

    created navbar component service

--- src/app/components/view-cart/cart.component.ts
+++ src/app/components/view-cart/cart.component.ts
@@ -1,30 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { CartService } from 'src/app/services/cart.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
-    cart: any;
-    id: any;
+export class CartComponent{
+    // cart: any;
+    // id: any;
 
-    constructor(private cartService: CartService, private route: ActivatedRoute) {}
+    // constructor(private cartService: CartService, private route: ActivatedRoute) {}
 
-    ngOnInit(): void {
-        this.oneCartDetail();
-    }
+    // ngOnInit(): void {
+    //     this.oneCartDetail();
+    // }
 
-    //fetch one cart detail from the services
-    oneCartDetail(){
-        if(this.id = this.route.snapshot.paramMap.get('id')) {
-            this.cartService.getOneCartDetail(this.id).subscribe((res) => {
-                this.cart = res
-            });
+    // //fetch one cart detail from the services
+    // oneCartDetail(){
+    //     if(this.id = this.route.snapshot.paramMap.get('id')) {
+    //         this.cartService.getOneCartDetail(this.id).subscribe((res) => {
+    //             this.cart = res
+    //         });
 
-        }
-    }
+    //     }
+    // }
 
 }
