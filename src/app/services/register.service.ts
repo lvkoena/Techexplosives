77187ffcfef3a81f43dae255d87e22ddb90846b0
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-08-22 14:52:55 +0200

    created echarts

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { User } from '../interface/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  // private baseUrl = 'http://localhost:8080/api/users'; // Adjust URL if necessary
+  private baseUrl = 'http://localhost:8080/api/users'; // Adjust URL if necessary
 
-  // constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  // getAllUsers(): Observable<User[]> {
-  //   return this.http.get<User[]>(this.baseUrl);
-  // }
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl);
+  }
 }
